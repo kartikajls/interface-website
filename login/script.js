1717c17bc1d449cda3202f61229070fa65b3dc19
kartika <bagimu.negeriku12@gmail.com>
2026-06-20 21:28:24 +0700

    statis

--- login/script.js
+++ login/script.js
@@ -5,8 +5,8 @@ document.getElementById("user-login").addEventListener("submit", function (event
     const password = document.getElementById("password1").value;
 
     if (username === "admin" && password === "admin123") {
-        window.location.href = "login.html";
+        alert("Sukses Login");
     } else {
-        window.location.href = "https://google.com";
+        alert("Password Salah");
     }
 });
